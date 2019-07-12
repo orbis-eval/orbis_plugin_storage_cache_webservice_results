abcba7126f29c1c25b96b319dbfdaf00d2bbfbaf
fabod <2189668+fabod@users.noreply.github.com>
2019-07-12 11:14:11 +0200

    wip

--- orbis_plugin_storage_cache_webservice_results/__init__.py
+++ orbis_plugin_storage_cache_webservice_results/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 __name__ = "orbis_plugin_storage_cache_webservice_results"
-__version__ = "1.1.dev0"
+__version__ = "2.1.1"
 __author__ = "fabod"
 __year__ = "2019"
-__description__ = "The cache_webservice_results storage plugin for Orbis"
+__description__ = "The storage plugin to cache webservice results."
 __license__ = "GPL2"
 __min_python_version__ = "3.6"
 __requirements_file__ = "requirements.txt"
